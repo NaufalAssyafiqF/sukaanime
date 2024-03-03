21c88f299863585d147f7ae2a2de84db9a9b1882
NaufalAssyafiqF <assyafiqnaufal625@gmail.com>
2024-03-03 23:27:26 +0700

    fix bug modul

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import HeroSlider from "./components/HeroSlider/HeroSlider";
-import AnimeList from "./components/animeList/animeList";
+import AnimeList from "./components/animeList/AnimeList";
 
 export default function Home() {
   return (
